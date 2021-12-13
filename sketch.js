fc256bb8f3360e054d830f41f87a2b1606ea4f7c
abhistm <95760465+abhistm@users.noreply.github.com>
2021-12-13 20:05:55 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,33 @@
+var ground ;
+var trex ,trex_running;
+
+function preload()
+{
+   trex_running=loadAnimation("trex1.png","trex3.png","trex4.png");
+
+}
+
+function setup()
+{
+  createCanvas(600,200)
+  ground=createSprite (300,190,600,20)  
+  //create a trex sprite
+  trex=createSprite(50,160,20,20);
+  trex.addAnimation("running",trex_running);
+  trex.scale=0.5;
+}
+
+
+function draw()
+{
+  background("lightgrey")
+ if(keyDown("space")){
+   trex.velocityY= -10 
+ } 
+ trex.velocityY=trex.velocityY+0.5
+  trex.collide(ground)
+ 
+ 
+ drawSprites();
+ 
+}
